39f3d70abd2cfa96f68bf183f2c64f89211b0cbe
jungai <juworaphol125@gmail.com>
2019-04-26 14:38:45 +0700

    timemachine 🕔

--- app/controllers/convert_data.spec.ts
+++
@@ -1,5 +0,0 @@
-import { convertTypeToInt } from './convert_data';
-
-test('return all type number', () => {
-  expect(convertTypeToInt({choosen: '1', value1: '2', value2: '3'})).toEqual({c1: 1, c2: 2, c3: 3});
-});
--- app/models/choice.ts
+++
@@ -1,6 +0,0 @@
-export enum Choice {
-  add = 1,
-  minus = 2,
-  multiply = 3,
-  divide = 4,
-}
